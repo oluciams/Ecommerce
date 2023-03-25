01e6d896958294d1e420b5e18182ee604b143655
oluciams <oluciams84@gmail.com>
2023-03-25 16:46:07 -0500

    add CheckCart as component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,7 @@ import styles from "../styles.module.css";
 import { RootState } from "../redux-store/store";
 import { getProducts } from "../redux-store/reducer/slices/productsSlice";
 import { toggleSelect } from "../utils/toggleSelect";
+import { CheckCart } from "./Icons";
 
 
 export const Card = ({
@@ -37,32 +38,16 @@ export const Card = ({
           <aside>
             {selected ? (
               <button className={styles["button-addCart"]}>
-                <svg
-                  width="22"
-                  height="17"
-                  viewBox="0 0 22 17"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M0.877104 6.28555C1.65815 5.50451 2.92448 5.50451 3.70553 6.28555L7.94817 10.5282L17.8477 0.6287C18.6287 -0.152349 19.895 -0.152348 20.6761 0.6287C21.4571 1.40975 21.4571 2.67608 20.6761 3.45713L7.94817 16.185L0.877104 9.11398C0.0960556 8.33293 0.0960556 7.0666 0.877104 6.28555Z"
-                    fill="white"
-                  />
-                </svg>
+                <CheckCart />
                 In Cart
               </button>
             ) : (
-              <>
-                <button
-                  onClick={() =>
-                    changeButton({ id, title, price, image })
-                  }
-                >
-                  Add To Cart
-                </button>
-              </>
+              <button
+                onClick={() => changeButton({ id, title, price, image })}
+              >
+                Add To Cart
+              </button>
+              
             )}
           </aside>
         </aside>
